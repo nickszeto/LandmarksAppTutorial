0a2a54f22aa15cc8a1f486f8e17067a43a057c9c
Nicholas Szeto <szetonick@gmail.com>
2021-07-27 21:49:40 +0800

    Test Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Nicholas Szeto on 27/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Nicholas!")
+            .padding()
+            .font(.title)
+            .foregroundColor(.green)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
