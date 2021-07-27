b892a93bba12c9eb6921ce6fd248d6d9ee572747
Nicholas Szeto <szetonick@gmail.com>
2021-07-27 22:00:15 +0800

    Commit to main

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
         Text("Hello, Nicholas!")
             .padding()
             .font(.title)
-            .foregroundColor(.blue)
+            .foregroundColor(.green)
     }
 }
 
