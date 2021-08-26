04db41175d99ffefb16004ef2044529125bc8764
Nicholas Szeto <szetonick@gmail.com>
2021-08-26 13:39:04 +0800

    26 Aug - Commit

---
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,39 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Nicholas Szeto on 19/8/21.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    @EnvironmentObject var modelData: ModelData
+    
+    var body: some View {
+        NavigationView {
+            List {
+                modelData.features[0].image
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height:200)
+                    .clipped()
+                    .listRowInsets(EdgeInsets())
+                
+                ForEach(modelData.categories.keys.sorted(), id:\.self) { key in
+                    CategoryRow(categoryName: key, items: modelData.categories[key]!)
+                }
+                .listRowInsets(EdgeInsets())
+            }
+            .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+            .environmentObject(ModelData())
+            
+    }
+}
